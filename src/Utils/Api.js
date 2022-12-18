69b4935f42392850edf293d78f2f309459bcdc9b
sukantad <ghoshsu4@gmail.com>
2022-12-19 00:14:50 +0530

    updated

--- src/Utils/Api.js
+++ src/Utils/Api.js
@@ -4,7 +4,7 @@ const baseUrl="https://youtube138.p.rapidapi.com";
 const options = {
     params: { hl: 'en', gl: 'US'},
     headers: {
-      'X-RapidAPI-Key':process.env.REACT_APP_YOUTUBE_API,
+      'X-RapidAPI-Key':"a0d7ebb156mshd6f97f6417cd652p1e7c03jsnd95f19362e47",
       'X-RapidAPI-Host': 'youtube138.p.rapidapi.com'
     }
 };
